d713ceb86a0062c739afb3750c1a2691f6765d00
yuktnk <yuki0wl@gmail.com>
2023-08-11 14:47:15 +0900

    ScrollView→FlatList

--- App.js
+++ App.js
@@ -1,23 +1,16 @@
 import { StatusBar } from "expo-status-bar"
-import { SafeAreaView, ScrollView, StyleSheet } from "react-native"
+import { SafeAreaView, StyleSheet, FlatList } from "react-native"
 import { ListItem } from "./components/ListItems"
 import articles from "./dummies/articles"
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-        {articles.map((article, index) => {
-          return (
-            <ListItem
-              imageUrl={article.urlToImage}
-              title={article.title}
-              author={article.author}
-              key={index.toString()}
-            />
-          )
-        })}
-      </ScrollView>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => <ListItem imageUrl={item.urlToImage} title={item.title} author={item.author} />}
+        keyExtractor={(item, index) => index.toString()}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   )
