7741d7bbc73bfc3a9893042d04b79d4f537af92a
yuktnk <yuki0wl@gmail.com>
2023-08-11 13:43:25 +0900

    itemコンポーネント作成

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import { StatusBar } from "expo-status-bar"
-import { StyleSheet, Image, View } from "react-native"
+import { StyleSheet, Image, Text, View } from "react-native"
 
 export default function App() {
+  const sampleText =
+    "仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト仮のテキスト"
   return (
     <View style={styles.container}>
       <View style={styles.itemContainer}>
         <View style={styles.leftContainer}>
           <Image style={{ width: 100, height: 100 }} source={{ uri: "https://picsum.photos/id/10/300/300" }} />
         </View>
-        <View style={styles.rightContainer}></View>
+        <View style={styles.rightContainer}>
+          <Text style={styles.text} numberOfLines={3}>
+            {sampleText}
+          </Text>
+          <Text style={styles.subText}>ReactNews</Text>
+        </View>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -25,15 +32,21 @@ const styles = StyleSheet.create({
   itemContainer: {
     height: 100,
     width: "100%",
-    backgroundColor: "red",
     flexDirection: "row",
   },
   leftContainer: {
     width: 100,
-    backgroundColor: "green",
   },
   rightContainer: {
     flex: 1,
-    backgroundColor: "blue",
+    padding: 10,
+    justifyContent: "space-between",
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: "gray",
   },
 })
