0b9bd579c7e1b1581833f929d5509906566bdf15
yuktnk <yuki0wl@gmail.com>
2023-08-12 12:36:05 +0900

    タブにアイコンを表示

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { NavigationContainer } from "@react-navigation/native"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { FontAwesome } from "@expo/vector-icons"
+
 import { HomeScreen } from "./screens/HomeScreen"
 import { ArticleScreen } from "./screens/ArticleScreen"
 import { ClipScreen } from "./screens/ClipScreen"
 
 const Tab = createBottomTabNavigator()
 const Stack = createNativeStackNavigator()
+
+const screenOptions = ({ route }) => ({
+  tabBarIcon: ({ color, size }) => {
+    if (route.name === "HomeTab") {
+      return <FontAwesome name="home" size={size} color={color} />
+    } else if (route.name === "ClipTab") {
+      return <FontAwesome name="bookmark" size={size} color={color} />
+    }
+  },
+})
 
 const HomeStack = () => {
   return (
@@ -20,9 +32,9 @@ const HomeStack = () => {
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="HomeStack" component={HomeStack} options={{ headerShown: false }} />
-        <Tab.Screen name="ClipTab" component={ClipScreen} />
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen name="HomeTab" component={HomeStack} options={{ headerShown: false, title: "Home" }} />
+        <Tab.Screen name="ClipTab" component={ClipScreen} options={{ headerShown: false, title: "Clip" }} />
       </Tab.Navigator>
     </NavigationContainer>
   )
